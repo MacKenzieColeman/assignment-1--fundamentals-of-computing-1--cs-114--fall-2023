7a58742ba0f3bc4ea3c83eca396cf928faf3d331
MacKenzieColeman <macoleman@hartford.edu>
2023-09-18 18:56:27 -0400

    added initials when program is run

--- Assignment1.java
+++ Assignment1.java
@@ -3,5 +3,13 @@
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
+  System.out.println("MMMM           MMMM  CCCCCCCCCCCC");
+  System.out.println("MMMMM         MMMMM  CCCCCCCCCCCC");
+  System.out.println("MMMMMM       MMMMMM  CCC");
+  System.out.println("MMM MMM     MMM MMM  CCC");
+  System.out.println("MMM  MMMMMMMMM  MMM  CCC");
+  System.out.println("MMM   MMMMMMM   MMM  CCC");
+  System.out.println("MMM    MMMMM    MMM  CCCCCCCCCCCC");
+  System.out.println("MMM             MMM  CCCCCCCCCCCC");
   }
 }
