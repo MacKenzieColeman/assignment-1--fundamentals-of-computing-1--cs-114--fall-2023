42ae7090ec3b9669ecffbe08f248e5a4195ac59b
MacKenzieColeman <macoleman@hartford.edu>
2023-09-20 08:28:37 -0400

    Faster Initials

--- Assignment1.java
+++ Assignment1.java
@@ -11,14 +11,14 @@ public class Assignment1 {
 
     //Print Initials
 
-    System.out.println("MMMM           MMMM  CCCCCCCCCCCC");
-    System.out.println("MMMMM         MMMMM  CCCCCCCCCCCC");
-    System.out.println("MMMMMM       MMMMMM  CCC");
-    System.out.println("MMM MMM     MMM MMM  CCC");
-    System.out.println("MMM  MMMMMMMMM  MMM  CCC");
-    System.out.println("MMM   MMMMMMM   MMM  CCC");
-    System.out.println("MMM    MMMMM    MMM  CCCCCCCCCCCC");
-    System.out.println("MMM             MMM  CCCCCCCCCCCC");
+    System.out.println(" MMMM           MMMM  CCCCCCCCCCCC" +
+    "\n MMMMM         MMMMM  CCCCCCCCCCCC" +
+    "\n MMMMMM       MMMMMM  CCC" +
+    "\n MMM MMM     MMM MMM  CCC" +
+    "\n MMM  MMMMMMMMM  MMM  CCC" +
+    "\n MMM   MMMMMMM   MMM  CCC" +
+    "\n MMM    MMMMM    MMM  CCCCCCCCCCCC" +
+    "\n MMM             MMM  CCCCCCCCCCCC");
 
     //Fahrenheit to Celsius Converter
 
